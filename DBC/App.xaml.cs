474c51e58bbf9409721cf9b67709118d91d5e0cc
Ross Searle <ross-s@live.co.uk>
2024-07-19 18:04:36 +0100

    Add project files.

---
+++ DBC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DBC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
