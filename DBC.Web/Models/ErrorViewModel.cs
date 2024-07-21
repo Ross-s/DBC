59ffb96e63d71b0f54db76c529c2afb75c930eaa
Ross Searle <ross-s@live.co.uk>
2024-07-21 19:48:15 +0100

    Changed DBC.Web to MVC

---
+++ DBC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
